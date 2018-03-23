5d0daea243ad5f8f25789a6b559036f266795802
Erno Wever <ernowever@Ernos-MacBook-Pro.local>
2018-03-23 03:54:14 +0100

    Add content

--- utils/_decks.js
+++ utils/_decks.js
@@ -5,7 +5,8 @@ export const DECKS_STORAGE_KEY = 'Flashcards:decks'
 
 export const setDummyData = (results) => {
   const id1 = uuid()
-  const id2 = '123'
+  const id2 = uuid()
+  const id3 = uuid()
 
   const dummyData = {
     [id1]: {
@@ -23,8 +24,54 @@ export const setDummyData = (results) => {
       ],
     },
     [id2]: {
-      title: 'JavaScript',
+      title: 'Trivia',
       id: id2,
+      questions: [
+        {
+          question: 'What is a group of lions called?',
+          answer: 'A pride',
+        },
+        {
+          question: 'According to the bible, who is the disciple that betrayed Jesus?',
+          answer: 'Judas Iscariot',
+        },
+        {
+          question: 'What is the name for trees that never lose their leaves?',
+          answer: 'Evergreen',
+        },
+        {
+          question: 'According to Greek mythology, who was the goddess of beauty?',
+          answer: 'Aphrodite',
+        },
+        {
+          question: 'What capital city lies on the Potomac Rive?',
+          answer: 'Washington D.C.',
+        },
+        {
+          question: 'What is the oldest city in the United States?',
+          answer: 'Saint Augustine, Florida',
+        },
+        {
+          question: 'Which music group has received the most Grammy Awards?',
+          answer: 'U2',
+        },
+        {
+          question: 'What hills border Scotland and England?',
+          answer: 'Cheviot Hills',
+        },
+        {
+          question: 'The first person shooter video game Doom was first released in what year?',
+          answer: '1993',
+        },
+        {
+          question: 'What are the four main ingredients in beer?',
+          answer: 'Grain, hops, yeast, and water',
+        },
+      ],
+    },
+    [id3]: {
+      title: 'JavaScript',
+      id: id3,
       questions: [
         {
           question: 'What is a closure?',
